36f18b434ea711347cf57e0438db96b84bd94e9c
biduzilla <rlyowlly19@gmail.com>
2022-11-11 16:31:39 -0300

    tela cadastro

--- projeto2/src/app/home/home-routing.module.ts
+++ projeto2/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent,
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent,
       }
     ]
   },
